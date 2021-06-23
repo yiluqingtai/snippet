8e4e47d3dcc71be242bae5e25414ce7c5e4b9985
yiluqingtai <1572236483@qq.com>
2021-06-23 19:08:16 +0800

    6.23

---
+++ Go/ch1/echo2/main.go
@@ -0,0 +1,28 @@
+/**
+ *
+ * File:    ch1/echo2/main.go
+ *			更常见的for循环写法
+ *			使用range关键词遍历集合，返回索引和对应的元素值
+ *			这里不需要索引，使用_作为占位符
+ *			另一种声明变量的方法: 使用:=符号
+ * 
+ * Author:  yiluqingtai(1572236483@qq.com)
+ *          Created on 21/6/23
+ *          
+ **/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	tmp, res := "", ""
+	for _, arg := range os.Args[1:] {
+		res += tmp + arg
+		tmp = " "
+	}
+	fmt.Println(res)
+}
---
+++ Go/ch1/helloworld/main.go
@@ -0,0 +1,28 @@
+/**
+ *
+ * File:    ch1/helloworld/main.go
+ *			go hello world实例
+ *			从main函数开始执行，必须要有main函数
+ *			打包为main
+ *			直接运行：go run main.go
+ *			编译：go build main.go
+ *			网上获取代码并编译：go get gopl.io/ch1/helloworld
+ * 
+ * Author:  yiluqingtai(1572236483@qq.com)
+ *          Created on 21/6/18
+ *          
+ **/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world!")
+}
+
+
+
+
+ 
+ 
